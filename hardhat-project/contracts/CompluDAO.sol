42777ce716816c55c16151f8d95f483fbdd923ef
josealonso <jose.alonso.programmer@gmail.com>
2022-05-26 19:44:10 +0200

    Created all the data structure

--- hardhat-project/contracts/CompluDAO.sol
+++ hardhat-project/contracts/CompluDAO.sol
@@ -43,10 +43,29 @@ interface ICompluNFT {
 }
 
 contract CompluDAO is Ownable {
-   
-   ICompluNFT compluNFT;
-   IFakeNFTMarketplace fakeNFTMarketplace;
-    /// @dev Maintain a mapping of Fake TokenID to Owner addresses
-    // mapping(uint256 => address) public tokens;
-    /// @dev Set the purchase price for each Fake NFT
+    struct Proposal {
+        // nftTokenId - the tokenID of the NFT to purchase from FakeNFTMarketplace if the proposal passes
+        uint256 nftTokenId;
+        // deadline - the UNIX timestamp until which this proposal is active. Proposal can be executed after the deadline has been exceeded.
+        uint256 deadline;
+        uint256 yesVotes;
+        uint256 noVotes;
+        // executed - whether or not this proposal has been executed yet. Cannot be executed before the deadline has been exceeded.
+        bool executed;
+        // voters - a mapping of CompluNFT tokenIDs to booleans indicating whether that NFT has already been used to cast a vote or not
+        mapping(address => bool) voters;
+    }
+
+    // Create a mapping of ID to Proposal
+    mapping(uint256 => Proposal) public proposals;
+    // Number of proposals that have been created
+    uint256 public numProposals;
+
+    ICompluNFT compluNFT;
+    IFakeNFTMarketplace nftMarketplace;
+
+    constructor(address _compluNFT, address _fakeNFTMarketplace) payable {
+        compluNFT = ICompluNFT(_compluNFT);
+        nftMarketplace = IFakeNFTMarketplace(_fakeNFTMarketplace);
+    }
 }
