f22068390f11f13f64372d4e161f9b63e968f911
josealonso <jose.alonso.programmer@gmail.com>
2022-05-27 19:38:15 +0200

    Implemented the core functionality

--- hardhat-project/contracts/CompluDAO.sol
+++ hardhat-project/contracts/CompluDAO.sol
@@ -53,7 +53,13 @@ contract CompluDAO is Ownable {
         // executed - whether or not this proposal has been executed yet. Cannot be executed before the deadline has been exceeded.
         bool executed;
         // voters - a mapping of CompluNFT tokenIDs to booleans indicating whether that NFT has already been used to cast a vote or not
-        mapping(address => bool) voters;
+        mapping(uint256 => bool) voters;
+        // mapping(address => bool) voters;
+    }
+
+    enum Vote {
+        YES, // 0
+        NO // 1
     }
 
     // Create a mapping of ID to Proposal
@@ -67,5 +73,96 @@ contract CompluDAO is Ownable {
     constructor(address _compluNFT, address _fakeNFTMarketplace) payable {
         compluNFT = ICompluNFT(_compluNFT);
         nftMarketplace = IFakeNFTMarketplace(_fakeNFTMarketplace);
+    }
+
+    // Create a modifier which only allows a function to be
+    // called by someone who owns at least 1 CompluNFT
+    modifier nftHolderOnly() {
+        require(compluNFT.balanceOf(msg.sender) > 0, "NOT_A_DAO_MEMBER");
+        _;
+    }
+
+    /// @dev createProposal allows a CompluNFT holder to create a new proposal in the DAO
+    /// @param _nftTokenId - the tokenID of the NFT to be purchased from FakeNFTMarketplace if this proposal passes
+    /// @return Returns the proposal index for the newly created proposal
+    function createProposal(uint256 _nftTokenId)
+        external
+        nftHolderOnly
+        returns (uint256)
+    {
+        require(nftMarketplace.available(_nftTokenId), "");
+        Proposal storage proposal = proposals[numProposals]; // IMPORTANT !!
+        proposal.nftTokenId = _nftTokenId;
+        // Set the proposal's voting deadline to be (current time + 5 minutes)
+        proposal.deadline = block.timestamp + 5 minutes;
+        // proposals[numProposals] = proposal;
+        numProposals++;
+        return numProposals - 1;
+    }
+
+    modifier activeProposalOnly(uint256 proposalIndex) {
+        require(
+            proposals[proposalIndex].deadline > block.timestamp,
+            "DEADLINE_EXCEEDED"
+        );
+        _;
+    }
+
+    /// @dev voteOnProposal allows a CompluNFT holder to cast their vote on an active proposal
+    /// @param proposalIndex - the index of the proposal to vote on in the proposals array
+    /// @param vote - the type of vote they want to cast
+    function voteOnProposal(uint256 proposalIndex, Vote vote)
+        external
+        nftHolderOnly
+        activeProposalOnly(proposalIndex)
+    {
+        Proposal storage proposal = proposals[proposalIndex];
+
+        uint256 numVotes = 0;
+        uint256 tokenId;
+        // Calculate how many NFTs are owned by the voter
+        // that haven't already been used for voting on this proposal
+        for (uint256 i = 0; i < compluNFT.balanceOf(msg.sender); i++) {
+            tokenId = compluNFT.tokenOfOwnerByIndex(msg.sender, i);
+            if (proposal.voters[tokenId] == false) {
+                numVotes++;
+                proposal.voters[tokenId] = true;
+            }
+        } // End of for loop
+        require(numVotes > 0, "ALREADY_VOTED");
+
+        if (vote == Vote.YES) {
+            proposal.yesVotes++;
+        } else if (vote == Vote.NO) {
+            proposal.noVotes++;
+        }
+    }
+
+    modifier inactiveProposalOnly(uint256 proposalIndex) {
+        require(
+            proposals[proposalIndex].deadline <= block.timestamp,
+            "DEADLINE_NOT_EXCEEDED"
+        );
+        require(
+            proposals[proposalIndex].executed == false,
+            "PROPOSAL_ALREADY_EXECUTED"
+        );
+        _;
+    }
+
+    function executeProposal(uint256 proposalIndex)
+        external
+        nftHolderOnly
+        activeProposalOnly(proposalIndex)
+    {
+        Proposal storage proposal = proposals[proposalIndex];
+
+        // call{amount: amount}();
+        if (proposal.yesVotes > proposal.noVotes) {
+            uint256 nftPrice = nftMarketplace.getPrice();
+            require(address(this).balance >= nftPrice, "NOT_ENOUGH_FUNDS");
+            nftMarketplace.purchase{value: nftPrice}(proposal.nftTokenId);
+        }
+        proposal.executed = true;
     }
 }
